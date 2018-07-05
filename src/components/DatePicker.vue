4ddfd8d144fb611e3a5cc67aed1ffccaff28a451
Lennie <lenny0307@gmail.com>
2018-07-04 21:33:29 -0400

    Project Initialization

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Todo: Build a datepicker</div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+
---
+++ tests/unit/DatePicker.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from '@vue/test-utils';
+import DatePicker from '@/components/DatePicker.vue';
+
+describe('DatePicker.vue', () => {
+  it('renders an input tag', () => {
+    // const msg = 'new message';
+    // const wrapper = shallowMount(HelloWorld, {
+    //   propsData: { msg },
+    // });
+    // expect(wrapper.text()).toMatch(msg);
+  });
+  it('displays a datepicker when input is clicked', () => {
+
+  });
+});
