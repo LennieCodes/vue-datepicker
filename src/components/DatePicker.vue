bb1d21e2e100d51aa488d2b607ba0aaedb3c20ba
LennieCodes <l.pinskiy@yahoo.com>
2018-07-09 07:54:15 -0400

    Added more boilerplate HTML for datepicker.

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <input type="text" />
+  <div class="dp-container">
+    <input type="text" @click="activateDatePicker" />
+    <dp-dropdown date-picker-active="datepickerActive"></dp-dropdown>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    showDatePicker() {
+import DatePickerDropdown from '@/components/DatePickerDropdown.vue';
 
+export default {
+  data() {
+    return {
+      datepickerActive: false,
+    };
+  },
+  components: {
+    'dp-dropdown': DatePickerDropdown,
+  },
+  methods: {
+    activateDatePicker() {
+      this.datepickerActive = !this.datepickerActive;
     }
   },
 };
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  .dp-container {
+    position: relative;
+  }
 </style>
---
+++ src/components/DatePickerDropdown.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="dp-dropdown" v-if="datepickerActive">
+      <div class="dp-dropdown-header">
+        <button class="prev-btn" @click="prevMonth">prev</button>
+        <span>March</span>
+        <button class="next-btn" @click="nextMonth">next</button>
+      </div>
+      <div class="dp-dropdown-body">
+        <button @click="selectDate">Click me</button>
+      </div>
+    </div>
+</template>
+
+<script>
+/*
+  What now? Need to setup date somehow? 
+
+  < month >
+*/
+export default {
+  props: {
+    datepickerActive: boolean,
+  },
+  methods: {
+    prevMonth() {
+
+    },
+    nextMonth() {
+
+    },
+    selectDate() {
+        
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .dp-dropdown {
+    position: absolute;
+    max-width: 400px;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    .dp-dropdown-header {
+      border-bottom: 1px grey solid;
+      .prev-btn {
+        float: left;
+      }
+      .next-btn {
+        float: right;
+      }
+    }
+  }
+</style>
